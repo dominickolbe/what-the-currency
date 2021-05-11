7c81a6a01df12930274f1d77e2281d552647a3e8
Dominic Kolbe <kolbedominic@gmail.com>
2021-05-11 02:25:36 +0100

    Fix api

--- src/components/ApiController/index.tsx
+++ src/components/ApiController/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { createErr, createOk } from "option-t/cjs/PlainResult";
-import { EXCHANGE_API, EXCHANGE_API_KEY } from "../../contstants";
+import { EXCHANGE_API_URL, EXCHANGE_API_KEY } from "../../contstants";
 import { RtExchangeRatesResponse } from "../../models/runtypes";
 import { LocalStorageController } from "../../utils";
 
@@ -11,15 +11,15 @@ export const ApiController = {
   get: {
     exchangerates: async () => {
       try {
-        const cache = LocalStorageController.get(EXCHANGE_API_KEY);
+        const cache = LocalStorageController.get("cache");
         if (cache.ok) return createOk(cache.val);
 
         const response = await axios.get(
-          `${EXCHANGE_API}/latest?base=${EXCHANGE_BASE}&symbols=${EXCHANGE_SYMBOLS}`
+          `${EXCHANGE_API_URL}/latest?base=${EXCHANGE_BASE}&symbols=${EXCHANGE_SYMBOLS}&access_key=${EXCHANGE_API_KEY}`
         );
         const result = RtExchangeRatesResponse.check(response.data);
 
-        LocalStorageController.set(EXCHANGE_API_KEY, result);
+        LocalStorageController.set("cache", result);
 
         return createOk(result);
       } catch (error) {
--- src/contstants.ts
+++ src/contstants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = process.env.REACT_APP_VERSION;
-export const EXCHANGE_API = process.env.REACT_APP_EXCHANGE_API;
+export const EXCHANGE_API_URL = process.env.REACT_APP_EXCHANGE_API_URL;
+export const EXCHANGE_API_KEY = process.env.REACT_APP_EXCHANGE_API_KEY;
 
 export const EXCHANGE_API_TLL = 60000 * 15; // in min
-export const EXCHANGE_API_KEY = "EXCHANGE_API_RESPONSE";
